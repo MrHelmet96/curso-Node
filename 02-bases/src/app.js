8d7b453da6cc014d78786ee00314c5c42da58c13
Alejandro <alejandromatiascasco@gmail.com>
2024-07-25 20:00:18 -0300

    manejando promesas

--- 02-bases/src/app.js
+++ 02-bases/src/app.js
@@ -1,15 +1,26 @@
-const {getAge, getUUId} = require('./plugins')
-
+//const {getAge, getUUId} = require('./plugins')
 //const {emailTemplate} = require('./js-foundation/01-template');
 //require('./js-foundation/02-destructuring');
 // const {getUserById} = require('./js-foundation/03-callbacks');
 // const {getUserById} = require('./js-foundation/04-arrow');
-const { buildMakePerson } = require('./js-foundation/05-factory');
 
+const getPokemonById = require('./js-foundation/06-promises');
+
+getPokemonById(669)
+    .then((pokemon) => console.log({pokemon}))
+    .catch((error) => console.log({error}))
+    .finally( () => console.log('Finalmente'));
+
+
+
+
+
+
+
+// ! ejercicio de factory functions
+/*const { buildMakePerson } = require('./js-foundation/05-factory');
 const makePerson = buildMakePerson({ getUUId, getAge});
-
 const obj = {name: 'Juan', birthdate: '1995-6-28'};
-
 const john = makePerson(obj);
+console.log({john});*/
 
-console.log({john});
