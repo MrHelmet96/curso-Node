342177ed8cc2a935e596d8e74f099ff6c9a09cb8
Alejandro <alejandromatiascasco@gmail.com>
2024-07-30 20:06:06 -0300

    starting with typescript

---
+++ 03-typescript/src/app.ts
@@ -0,0 +1,24 @@
+const heroes = [
+{
+    id: 1,
+    name: 'Batman',
+    owner: 'DC',
+},
+{
+    id: 2,
+    name: 'Spiderman',
+    owner: 'Marvel',
+},
+{
+    id: 3,
+    name: 'Superman',
+    owner: 'DC',
+}];
+
+const findHeroById = (id : number) => {
+    return heroes.find((hero) => hero.id === id);
+}
+
+const hero = findHeroById(4);
+
+console.log(hero?.name ?? 'Hero not found!!');
