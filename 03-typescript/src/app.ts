8612702eba026612f19dba076ea1e9d396426d3f
Alejandro <alejandromatiascasco@gmail.com>
2024-07-31 11:27:57 -0300

    practicando typescript

--- 03-typescript/src/app.ts
+++ 03-typescript/src/app.ts
@@ -1,23 +1,5 @@
-const heroes = [
-{
-    id: 1,
-    name: 'Batman',
-    owner: 'DC',
-},
-{
-    id: 2,
-    name: 'Spiderman',
-    owner: 'Marvel',
-},
-{
-    id: 3,
-    name: 'Superman',
-    owner: 'DC',
-}];
+import { findHeroById } from "./services/hero.service";
 
-const findHeroById = (id : number) => {
-    return heroes.find((hero) => hero.id === id);
-}
 
 const hero = findHeroById(1);
 
---
+++ 03-typescript/src/services/hero.service.ts
@@ -0,0 +1,9 @@
+import { heroes } from "../data/heroes";
+
+
+
+export const findHeroById = (id : number) => {
+    return heroes.find((hero) => hero.id === id);
+}
+
+
