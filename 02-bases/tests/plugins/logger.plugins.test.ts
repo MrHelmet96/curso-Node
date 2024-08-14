edcecb55effe0d76940d77bf8aa58962f19c619b
Alejandro <alejandromatiascasco@gmail.com>
2024-08-14 10:55:14 -0300

    Finalizando Testing

--- 02-bases/tests/plugins/logger.plugins.test.ts
+++ 02-bases/tests/plugins/logger.plugins.test.ts
@@ -1,4 +1,4 @@
-import { buildLogger } from "../../src/plugins";
+import { buildLogger, logger as winstonLogger } from "../../src/plugins/logger.plugins";
 
 
 
@@ -8,5 +8,26 @@ describe('plugins/logger.plugins.ts', () => {
         const logger = buildLogger('test');
         expect(typeof logger.log).toBe('function');
         expect(typeof logger.error).toBe('function');
+    });
+
+    test('logger.log should log a message', () => {
+
+        const winstonLoggerMock = jest.spyOn(winstonLogger, 'log');
+
+        const message = 'test message';
+        const service = 'test service';
+
+        const logger = buildLogger(service);
+
+        logger.log(message);
+
+        expect( winstonLoggerMock ).toHaveBeenCalledWith(
+            'info',
+            expect.objectContaining({
+                level:'info',
+                message,
+                service,}),
+            );
+
     });
 });
