4975215226a5150d1117f39c7569749990d4265c
Alejandro <alejandromatiascasco@gmail.com>
2024-08-17 19:31:46 -0300

    probando creación de archivos

--- 04-multiplication/src/app.ts
+++ 04-multiplication/src/app.ts
@@ -1,5 +1,22 @@
+import fs from 'fs';
 
 
-const message:string = 'Hola Mundo';
+let outputsMessage = '';
+const base = 5;
+const headerMessage = `====================================\n          Tabla del ${base}\n====================================\n`;
 
-console.log(message);
+    for (let i = 1; i <= 10; i++) {
+        outputsMessage += `${base} x ${i} = ${base*i}\n`;
+    }
+
+outputsMessage = headerMessage + outputsMessage;
+
+const outputPath = `outputs`;
+
+//en consola
+console.log(outputsMessage);
+
+//en archivo
+fs.mkdirSync(outputPath, { recursive: true });
+fs.writeFileSync(`${outputPath}/tabla-${base}.txt`, outputsMessage);
+console.log("file created");
